951044d8d1cbcc339c9f4d437d74ad5d3311c3c0
apto-as <apto.as@gmail.com>
2020-05-26 11:40:46 +0900

    add jupyter files

---
+++ .jupyter/jupyter_notebook_config.py
@@ -0,0 +1,12 @@
+# Set options for certfile, ip, password, and toggle off
+# browser auto-opening
+#c.NotebookApp.certfile = u'/home/timoto/certificate/mycert.pem'
+#c.NotebookApp.keyfile = u'/home/timoto/certificate/mykey.key'
+# Set ip to '*' to bind on all interfaces (ips) for the public server
+c.NotebookApp.ip = '*'
+c.NotebookApp.password = u'sha1:38e1e2d49165:ecb83978baf0c1628b688b42ac5e37366918c14f'
+c.NotebookApp.open_browser = False
+
+# It is a good idea to set a known, fixed port for server access
+c.NotebookApp.port = 52103
+
