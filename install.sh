1897b34b7cbfb2d3490c4e0ccfc2dcdf59d52a67
apto_as <t.imoto@9dwjp>
2020-06-04 16:33:22 +0900

    create deploy.sh

--- install.sh
+++ install.sh
@@ -20,16 +20,7 @@ sh ~/.vim_runtime/install_awesome_vimrc.sh
 ln -snfv ~/dotfiles/my_configs.vim ~/.vim_runtime/
 
 #dotfiles install
-cd ~/dotfiles
-for f in .??*; do
-    [ "$f" = ".git" ] && continue
-    [ "$f" = ".gitconfig.local.template" ] && continue
-    [ "$f" = ".require_oh-my-zsh" ] && continue
-    [ "$f" = ".gitmodules" ] && continue
-
-    ln -snfv ~/dotfiles/"$f" ~/
-done
-cd ~/
+sh ~/dotfiles/deploy.sh
 
 #vim plug install
 vim +PlugInstall
@@ -40,6 +31,5 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo ".....install end!"
 echo ".....next command"
 echo "p10k configure"
-echo "wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh"
 
 
