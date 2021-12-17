e2a0218e089eccc47924096ab7146133be400fdf
Tsuyoshi IMOTO <apto.as@gmail.com>
2021-12-17 23:42:29 +0900

    add bobthefish configs.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -72,7 +72,16 @@ set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme dracula 
 set -g theme_newline_cursor yes
-set -g theme_newline_prompt '$ '
+
+set -g theme_display_docker_machine yes
+set -g theme_display_k8s_context yes
+set -g theme_display_hg yes
+set -g theme_display_virtualenv yes
+set -g theme_display_node yes
+set -g theme_date_format "+%a %H:%M"
+set -g theme_avoid_ambiguous_glyphs yes
+set -g theme_show_exit_status yes
+set -g theme_display_jobs_verbose yes
 
 switch (uname)
   case Darwin
