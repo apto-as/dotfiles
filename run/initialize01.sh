b0487e47c78c497d09c1326a851f3a931ae76104
Tsuyoshi IMOTO <apto.as@gmail.com>
2022-04-20 16:59:17 +0900

    separate docker initializetion.

--- run/initialize01.sh
+++ run/initialize01.sh
@@ -9,21 +9,6 @@ sudo apt install -y wget curl ufw peco mosh golang fish neovim nodejs tmux cargo
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# nvidia-docker install
-# install docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo usermod -aG docker $USER
-sudo apt install -y docker-compose
-
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-
-sudo apt update
-sudo apt install -y nvidia-docker2
-sudo systemctl restart docker
-
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
---
+++ run/initialize_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# nvidia-docker install
+# install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo usermod -aG docker $USER
+sudo apt install -y docker-compose
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt update
+sudo apt install -y nvidia-docker2
+sudo systemctl restart docker
+
