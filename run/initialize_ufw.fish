29f87ca8b13c1763f7a385c87a0f1553cdd41d9d
Tsuyoshi IMOTO <apto.as@gmail.com>
2022-04-25 12:44:40 +0900

    add vnc ports

--- run/initialize_ufw.fish
+++ run/initialize_ufw.fish
@@ -2,6 +2,10 @@
 
 sudo ufw allow 49233
 sudo ufw allow 49233/udp
+sudo ufw allow 5900
+sudo ufw allow 5900/udp
+sudo ufw allow 9999
+sudo ufw allow 9999/udp
 sudo ufw allow 60000:60001/udp
 
 sudo ufw logging on
