4aec0c8266ed1740f9c34868c27d77656b6b669a
Tsuyoshi IMOTO <apto.as@gmail.com>
2023-03-15 19:48:24 +0900

    add ufw ports

--- run/initialize_ufw.fish
+++ run/initialize_ufw.fish
@@ -8,8 +8,10 @@ sudo ufw allow 49234
 sudo ufw allow 49234/udp
 sudo ufw allow 49235
 sudo ufw allow 49235/udp
-sudo ufw allow 5900
-sudo ufw allow 5900/udp
+sudo ufw allow 7001
+sudo ufw allow 7001/udp
+sudo ufw allow 7860
+sudo ufw allow 7860/udp
 sudo ufw allow 9999
 sudo ufw allow 9999/udp
 sudo ufw allow 60000:60001/udp
